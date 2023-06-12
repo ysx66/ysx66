f8296d57db211dac4dc1c0b2b8556dc0516ce016
ysx88 <115683308@qq.com>
2023-06-13 02:18:32 +0800

    🤞 Sync 2023-06-13 02:18:32🍉

--- my-packages/scripts/packages.sh
+++ my-packages/scripts/packages.sh
@@ -15,15 +15,12 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge
 git clone --depth=1 https://github.com/kiddin9/luci-app-dnsfilter
 git clone --depth=1 https://github.com/kiddin9/qBittorrent-Enhanced-Edition
 git clone --depth=1 https://github.com/yaof2/luci-app-ikoolproxy
 git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 git clone --depth=1 https://github.com/ntlf9t/luci-app-easymesh
 git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb
 git clone --depth=1 https://github.com/sirpdboy/luci-app-advanced
@@ -32,7 +29,7 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns smartdns
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+git clone https://github.com/Hyy2001X/AutoBuild-Packages && rm -rf AutoBuild-Packages/luci-app-adguardhome && mvdir AutoBuild-Packages
 git clone --depth=1 https://github.com/peter-tank/luci-app-autorepeater
 git clone --depth=1 https://github.com/sensec/luci-app-udp2raw
 git clone --depth=1 https://github.com/sensec/openwrt-udp2raw udp2raw
@@ -41,7 +38,7 @@ git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk mentohu
 git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 git clone --depth=1 https://github.com/fw876/helloworld && rm -rf helloworld/luci-app-ssr-plus
-
+git clone https://github.com/kiddin9/openwrt-adguardhome && mvdir openwrt-adguardhome
 git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
 git clone --depth 1 https://github.com/sbwml/openwrt-alist && mvdir openwrt-alist
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns openwrt-mosdns && mvdir openwrt-mosdns
@@ -60,14 +57,12 @@ svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass
 svn export https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic
 svn export https://github.com/coolsnowwolf/packages/trunk/multimedia/UnblockNeteaseMusic
 svn export https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
-svn export https://github.com/immortalwrt/packages/trunk/net/adguardhome
 svn export https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/gost
 svn export https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/iptvhelper
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-server
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-pppoe-server
 svn export https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
-svn export https://github.com/281677160/openwrt-package/trunk/luci-app-cpulimit
 svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-webdav/luci-app-gowebdav
 svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-webdav/gowebdav
 svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox/luci-app-homebox
