7faf23aa642504904a1bb0fb21aa8f1fea09f7e2
ysx88 <115683308@qq.com>
2023-06-22 02:18:30 +0800

    🐶 Sync 2023-06-22 02:18:30🦄

--- shuoming/x86/diy-part1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# Add a feed source
-echo 'src-git ysx88 https://github.com/ysx88/my-packages' >>feeds.conf.default
-# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-# sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
